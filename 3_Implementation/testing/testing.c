d3722fe5a5632e75d4429ec812a440b5f4abd310
37-51 <sathvikthaduri3751@gmail.com>
2022-02-11 11:52:22 +0530

    third commit

--- 3_Implementation/testing/testing.c
+++ 3_Implementation/testing/testing.c
@@ -1,11 +1,11 @@
 #include "unity.h"
 #include "regist.h"
 
-#define PROJECT_NAME    "Vaccine_Registration"
+#define PROJECT_NAME    "Vehicle_Registration"
 
 // Prototypes 
 void test_regist_status(void);
-void test_vaccine_status(void);
+void test_vehicle_status(void);
 
 void setUp(){}
 
@@ -15,9 +15,9 @@ int main()
 {
   UNITY_BEGIN();
 
-// Run Test functions for registration and vaccine status
+// Run Test functions for registration and vehicle status
   RUN_TEST(test_regist_status);
-  RUN_TEST(test_vaccine_status);
+  RUN_TEST(test_vehicle_status);
   
   return UNITY_END();
 }
@@ -29,8 +29,8 @@ void test_regist_status(void) {
   TEST_ASSERT_EQUAL(-1, regist_status(150)); // Fail condition
 }
 
-void test_vaccine_status(void) {
-  TEST_ASSERT_EQUAL(1, vaccine_status(1));
+void test_vehicle_status(void) {
+  TEST_ASSERT_EQUAL(1, vehicle_status(1));
   
-  TEST_ASSERT_EQUAL(2, vaccine_status(1)); // Fail condition
+  TEST_ASSERT_EQUAL(2, vehicle_status(1)); // Fail condition
 }
