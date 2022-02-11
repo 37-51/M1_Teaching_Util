5361da98ff01f6aeae3e96782f66210816f5f020
37-51 <sathvikthaduri3751@gmail.com>
2022-02-11 11:49:44 +0530

    third

--- 3_Implementation/Sourcecode/registration.c
+++ 3_Implementation/Sourcecode/registration.c
@@ -3,12 +3,12 @@
 
 int regist_status(long p){
 
-    // Checks for registration details of new patients    
-    pd[0].pannumber=p;
+    // Checks for registration details of new users    
+    pd[0].vehiclenumber=p;
     int i=1;
     while(i<=1000){
-        if(pd[i].pannumber==pd[0].pannumber){
-            if(pd[i].vaccine_type==0)
+        if(pd[i].vehiclenumber==pd[0].vehiclenumber){
+            if(pd[i].direction_type==0)
                 return 0;
             else
                 return 1;
@@ -18,16 +18,16 @@ int regist_status(long p){
     return -1;
 }
 
-// Checks how many doses should be given
-int vaccine_status(int i){
+// Checks how many directions should be given
+int vehicle_status(int i){
      
-    if (pd[i].vaccine_doses==0){
-        pd[i].vaccine_doses++;
+    if (pd[i].user_tolls==0){
+        pd[i].user_tolls++;
         return 1;}
-    else if(pd[i].vaccine_doses==1){
-        pd[i].vaccine_doses++;
+    else if(pd[i].user_tolls==1){
+        pd[i].user_tolls++;
         return 2;}
-    else if (pd[i].vaccine_doses==2){
+    else if (pd[i].user_tolls==2){
         return 3;}
     else
         return 0;
